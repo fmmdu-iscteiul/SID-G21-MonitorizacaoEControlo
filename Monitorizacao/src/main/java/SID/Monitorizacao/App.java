80dd1216639b51bcd71e3fa67cae0ce2094a8bea
fmmdu-iscteiul <fmmdu@iscte-iul.pt>
2021-05-01 18:51:02 +0100

    Import do projeto

---
+++ Monitorizacao/src/main/java/SID/Monitorizacao/App.java
@@ -0,0 +1,13 @@
+package SID.Monitorizacao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
